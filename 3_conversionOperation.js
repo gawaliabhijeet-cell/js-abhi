09e95ac281d9542e7d5c54cef9ae437ccb46058a
Abhijeet Gawali <gawaliabhijeet-cell>
2026-02-24 14:54:45 +0530

    I stady conversion

---
+++ 3_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="34as"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuenumber =Number(score)
+console.log(typeof valuenumber)
+console.log(valuenumber)
+
+
+// "34" => 34
+// "33abc" => NaN (not a number)
+// true =>1; false =>0 
+
+
+let isLoggedIn =1
+let booleanlogin =Boolean(isLoggedIn)
+console.log(booleanlogin)
+
+// 1=> true; 0=> false
+// "" => false
+// "abhi" =>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
