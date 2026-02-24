8590ac9c9aea5e48c99c6c23e2b4f253d7dde886
Abhijeet Gawali <gawaliabhijeet-cell>
2026-02-24 15:33:46 +0530

    learned operations

--- 3_conversionOperation.js
+++ 3_conversionOperation.js
@@ -26,3 +26,39 @@ let somenumber=33
 let stringnumber=String(somenumber)
 console.log(stringnumber)
 console.log(typeof stringnumber)
+
+// ************************* operations **********************
+let value =3
+let negValue =-value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/2)
+// console.log(2%2)
+
+let s1="hi"
+let s2=" abhijeet"
+
+let s3=s1+s2
+console.log(s3)
+
+// console.log("1 +2")
+// console.log(1+"2")
+// console.log("2"+"3")
+// console.log(1+2+"2")
+// console.log("1"+2+4)
+
+// console.log(+true) // o/p => 1
+// console.log(+"") // o/p => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++
+
+console.log(gamecounter)
