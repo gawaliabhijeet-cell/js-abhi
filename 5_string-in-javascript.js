e608376a07b6431c8e06522c3da6e8074bea73d3
Abhijeet Gawali <gawaliabhijeet-cell>
2026-02-25 13:05:02 +0530

    information string

---
+++ 5_string-in-javascript.js
@@ -0,0 +1,12 @@
+const name = "abhi"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+console.log(`Hello my name is ${name} and my repo count
+ is ${repoCount}`);
+
+const gameName = new String("abhijeet")
+
+console.log(gameName[0])        // a
+console.log(gameName.__proto__) // String prototype object
