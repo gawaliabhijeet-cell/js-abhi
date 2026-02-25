7fd7ffa42787c42b2fdefa5704a9dbe177eb4a89
Abhijeet Gawali <gawaliabhijeet-cell>
2026-02-25 14:15:23 +0530

    string method information

--- 5_string-in-javascript.js
+++ 5_string-in-javascript.js
@@ -10,3 +10,26 @@ const gameName = new String("abhijeet")
 
 console.log(gameName[0])        // a
 console.log(gameName.__proto__) // String prototype object
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf("j"))
+
+ const newString = gameName.substring(0,4)
+ console.log(newString)
+
+ const anotherString = gameName.slice(-6,4)
+ console.log(anotherString)
+
+const newStringone ="  abhi   "
+console.log(newStringone)
+console.log(newStringone.trim())
+
+const url ="https://abhi.com/demo/bin"
+console.log(url.replace("demo","---"))
+
+console.log(url.includes("abhi"))
+
+console.log(gameName.split(""))  // split() always returns an array.
+
