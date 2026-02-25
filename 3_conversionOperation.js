ca511f2bdbb2cc3b27f13e1586bfff58f4fc8046
Abhijeet Gawali <gawaliabhijeet-cell>
2026-02-25 20:58:36 +0530

    basic js

---
+++ 3_conversionOperation.js
@@ -0,0 +1,64 @@
+let score="34as"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuenumber =Number(score)
+console.log(typeof valuenumber)
+console.log(valuenumber)
+
+
+// "34" => 34
+// "33abc" => NaN (not a number)
+// true =>1; false =>0 
+
+
+let isLoggedIn =1
+let booleanlogin =Boolean(isLoggedIn)
+console.log(booleanlogin)
+
+// 1=> true; 0=> false
+// "" => false
+// "abhi" =>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+// ************************* operations **********************
+let value =3
+let negValue =-value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/2)
+// console.log(2%2)
+
+let s1="hi"
+let s2=" abhijeet"
+
+let s3=s1+s2
+console.log(s3)
+
+// console.log("1 +2")
+// console.log(1+"2")
+// console.log("2"+"3")
+// console.log(1+2+"2")
+// console.log("1"+2+4)
+
+// console.log(+true) // o/p => 1
+// console.log(+"") // o/p => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++
+
+console.log(gamecounter)
