b1bd0a26f59e67032d18a762e7034fcf7be5205b
Abhijeet Gawali <gawaliabhijeet-cell>
2026-03-15 23:21:02 +0530

    basic

---
+++ 1-basic/2_datatype.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+// alert("hello")
+// alert(3+3) // we are using node.js not brower
+
+console.log(3+3); console.log("abhi") // code readability should be high
+
+let name ="abhi"
+let age =19
+let isloggedin=false
+let state;
+// number => 2 to power 53
+// bigint=> big number like account number
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=> unique value
+
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
