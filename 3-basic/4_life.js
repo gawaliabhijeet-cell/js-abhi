0edf441d6e35a76f13b8ed872c800b78224903e2
Abhijeet Gawali <gawaliabhijeet-cell>
2026-03-22 21:36:18 +0530

    IIFE

---
+++ 3-basic/4_life.js
@@ -0,0 +1,13 @@
+/* Immediately Invoked Function Expression (IIFE) */
+
+// interview question important
+(function naruto(){
+// name IIFE
+    console.log(` load 7`)
+} )(); // at the end semicolon dena jaruri hai
+
+((name) => {
+    console.log(`load 8 ${name}`)
+})("shikamaru");
+
+
