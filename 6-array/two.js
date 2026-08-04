53175f375bee87f589bb54f04dd15bfebe2902ed
Abhijeet Gawali <gawalishambu@gmail.com>
2026-08-04 18:12:40 +0530

    add initial implementation of array manipulation examples

---
+++ 6-array/two.js
@@ -0,0 +1,23 @@
+const mynumber = [45,54,87,61,5,55,445,878,87]
+
+// let num = mynumber.map((n) => {
+    // console.log(n + 10);
+//     return ++n;
+// })
+// console.log(num);
+
+// num = mynumber.forEach((n)=> {
+//     console.log(n--);
+//     console.log(n);
+    
+// })
+
+/*
+const number = mynumber
+.map((n) => n * 10) // return  all 
+.map((n) => n + 1)
+.filter( (n) => n >= 500) // return true value
+
+console.log(number);
+*/
+
